5b85d4fdf21908c8d948a07009292f7dc38e0d77
Julien Frisch <julien_frisch@outlook.com>
2016-06-23 15:30:47 -0500

    Changed Name to TipTapTouch

---
+++ TipTapTop/TouchPoint.swift
@@ -0,0 +1,13 @@
+//
+//  TouchPoint.swift
+//  TipTapTop
+//
+//  Created by Julien Frisch on 6/23/16.
+//  Copyright © 2016 Julien Frisch. All rights reserved.
+//
+
+import Cocoa
+
+class TouchPoint: SKSpriteNode {
+
+}
